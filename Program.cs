f641881303e5ccc942493385e4c1abdbbc4e09cd
sattyamkishormishra <77384335+sattyamkishormishra@users.noreply.github.com>
2021-02-04 17:07:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using log4net;
+
+namespace FIrstBlazorApplication
+{
+    public class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Style.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FIrstBlazorApplication
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class Style : ControllerBase
+    {
+        private IConfiguration configuration { get; }
+
+        public Style(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+        [HttpGet]
+        public async Task<ActionResult> ChangeStyle ([FromQuery] string style)
+        {
+            string TmcCodeValue = configuration.GetSection("TMCCode").Value;
+            Response.Cookies.Append("TMCCode", TmcCodeValue);
+            return Redirect("/");
+
+        }
+    }
+}
